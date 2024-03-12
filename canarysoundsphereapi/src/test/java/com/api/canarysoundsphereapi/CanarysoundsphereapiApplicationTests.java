ad05ce49b1bb6796bc03f8aa58401eeb27fc3882
AIPONCEP <ana_ponce_perez@hotmail.es>
2024-03-12 17:20:09 +0100

    proyecto y carpetas api

---
+++ canarysoundsphereapi/src/test/java/com/api/canarysoundsphereapi/CanarysoundsphereapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.canarysoundsphereapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanarysoundsphereapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
